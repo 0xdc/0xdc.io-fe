c5ac9bd24565f7cca93a63501381e634f4fb7fc9
Daniel Cordero (bastion) <bastion@azolla.0xdc.host>
2019-04-26 08:13:04 +0000

    Randomise the generated email address

---
+++ frontend/static/js/fp-email.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addElementSelector("DOMContentLoaded", function(e){
+	var generated_address = "website." + (Math.random() * 10 ** 16).toString(16) + "@" + window.location.host;
+	var a = document.createElement("a");
+	a.href = "mailto:" + generated_address;
+	a.classList = ["email"];
+	a.appendChild(document.createTextNode(generated_address));
+	document.querySelector("#hcard-0xdc").appendChild(a);
+})
