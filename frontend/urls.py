911ca2ad1daeb277fef420feb324d9f2e281e447
Daniel Cordero <website0@0xdc.io>
2019-08-25 14:25:31 +0000

    Convert urls to path
    Use path() from django-2, losing compat from python2

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = "frontend"
 urlpatterns = [
-        url(r'^$', views.homepage, name="index"),
+        path('', views.homepage, name="index"),
 ]
