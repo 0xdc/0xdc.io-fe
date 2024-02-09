183985bfb9c57148c9b3e010a83bfca987ad6f19
Daniel Cordero <frontend@0xdc.io>
2024-02-09 13:25:31 +0000

    change livestream provider to owncast

--- frontend/static/frontend/js/livestream.js
+++ frontend/static/frontend/js/livestream.js
@@ -4,14 +4,20 @@ var livestream = function(uid){
 	// check the existance of the hls playlist
 	// if found, place a video widget on the page
 	var header = document.querySelector("header");
-	fetch("/hls/" + uid + ".m3u8")
+	fetch("https://"+uid+"/api/status")
 	.then(response => {
 		if (!response.ok) {
 			throw new Error("http response not okay. not doing anything");
 		}
-		return response.url;
+		return response.json();
 	})
-	.then(url => {
+	.then(data => {
+		if (!data['online']) {
+			throw new Error("stream is offline. skipping");
+		}
+
+		var url = "https://"+uid+"/hls/stream.m3u8";
+
 		var video = document.createElement("video");
 		video.controls = true;
 		video.muted = true;
